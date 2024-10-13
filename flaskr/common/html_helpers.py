c4444c9443e32cdf6a2335c5ad7938031bc35aaf
Nurad2325 <darun23@gmail.com>
2024-10-13 14:00:06 -0700

    housekeeping 1

--- flaskr/common/html_helpers.py
+++ flaskr/common/html_helpers.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 
 def clean_html(html_content):
-    # Use a library like BeautifulSoup to strip HTML tags
+    #Use a library like BeautifulSoup to strip HTML tags
     soup = BeautifulSoup(html_content, 'html.parser')
     return soup.get_text()
