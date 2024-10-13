64700c8bed0891df133cc86a86a03622a95385a4
Nitinram Velraj <nvelraj@Nitinrams-MacBook-Air.local>
2024-10-12 17:15:52 -0700

    webhook

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev')
     LOG_TO_STDOUT = os.getenv('LOG_TO_STDOUT')
+    EXECUTOR_PROPAGATE_EXCEPTIONS = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
